95fd9557a979c613b4d1671d9b12f3250a07c348
Dragohn <coff33ninja69@gmail.com>
2025-01-31 00:59:28 +0200

    Oops! Missed This one.

--- main.py
+++ main.py
@@ -359,6 +359,6 @@ class AIChatApp(QMainWindow):
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = DeepSeekApp()
+    window = AIChatApp()
     window.show()
     sys.exit(app.exec())
